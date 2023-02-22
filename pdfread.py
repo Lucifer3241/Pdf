20716b980af58c3244d7f66b64cef36e181c6191
Divith N <71309861+Lucifer3241@users.noreply.github.com>
2023-02-22 16:37:41 +0530

    Update pdfread.py

--- pdfread.py
+++ pdfread.py
@@ -1,5 +1,6 @@
 import PyPDF2
 import os
+import pdfplumber
 import json
 import fitz
 
@@ -9,6 +10,7 @@ def size_check(file):
         print('file size is more than 5Mb')
         exit(0)
     else:
+        print('size ok')
         return
 
 def error_check(file):
@@ -18,6 +20,7 @@ def error_check(file):
         print('invalid PDF file')
         exit(0)
     else:
+        print('no error')
         pass
 
 def empty_check(file):
@@ -27,18 +30,47 @@ def empty_check(file):
         page=reader.pages[i]
         if(page.get_contents()==None):
             print('empty')
+            flag=0
         else:
+            flag=1
             pass
+    if(flag==1):
+        print('not empty')
 def extract_text(file):
-    reader=PyPDF2.PdfReader(file)
-    dictionary={}
-    totalpages = len(reader.pages)
-    for i in range(0, totalpages):
-        page=reader.pages[i]
-        pdfData=page.extract_text()
-        dictionary={**dictionary,**{'page '+str(i+1):pdfData}}
-    with open("text.json", "w") as outfile:
-        json.dump(dictionary, outfile)
+    
+    table=[]
+    with pdfplumber.open(file) as pdf:
+        totalpages = len(pdf.pages)
+        for i in range(0, totalpages):
+            page = pdf.pages[i]    
+            table.append(page.extract_tables(table_settings={"vertical_strategy": "lines", 
+                                                    "horizontal_strategy": "lines", 
+                                                    "snap_tolerance": 4,}))
+        for i in table:
+            for j in i:
+                for k in j:
+                    if k==['', None]:
+                        del j[j.index(k)]
+
+        dic={}
+
+        for i in table:
+            for j in i:
+                for k in j:
+                    for l in k:
+                        r=k.index(l)
+                
+                        if l =='':
+                            o=k.index(l)
+                            dic.update({head:dic[head]+', '+k[0+1]})
+                            break
+                        else:
+                            dic.update({k[r]: k[r+1]})
+                            head=k[r]
+                            break
+        
+        with open("text.json", "w") as outfile:
+            json.dump(dic, outfile)
     
 def extract_images(file):
     doc=fitz.open(file)
@@ -54,7 +86,7 @@ def extract_images(file):
                 pix1 = None
             pix = None
     
-file='sample5.pdf'
+file='Delhi_A_2022_10.pdf'
 size_check(file)
 error_check(file)
 empty_check(file)
